e38b4232e3dfe7796dcca0798368b80c4719c8a5
kislovskiy <kislovskiy@protonmail.com>
2023-10-12 14:22:41 +0200

    plot a dot using d3

--- kislovskiy--panel/src/components/SimplePanel.tsx
+++ kislovskiy--panel/src/components/SimplePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css } from '@emotion/css';
@@ -32,27 +32,47 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
   const styles = useStyles2(getStyles);
   const svgRef = useRef(null);
 
+  const fixedAxisLength = 10; // Define the fixed axis length
+
   useEffect(() => {
-    // Ensure we have data and the SVG container element
     if (data.series.length > 0 && svgRef.current) {
       const svg = d3.select(svgRef.current);
 
       // Clear any existing content in the SVG
       svg.selectAll('*').remove();
 
-      // Create the circle
-      svg
-        .append('circle')
-        .attr('cx', 0)
-        .attr('cy', 0)
-        .attr('r', 50)
-        .style('fill', theme.colors.primary.main);
+      // Create a group for the coordinate system
+      const coordinateSystem = svg
+        .append('g')
+        .attr('transform', `translate(40, ${height - 40})`);
 
-      svg
+      const xScale = d3.scaleLinear().domain([0, fixedAxisLength]).range([0, width]);
+      const xAxis = d3.axisBottom(xScale).ticks(fixedAxisLength);
+      coordinateSystem
+        .append('g')
+        .attr('class', 'x-axis')
+        .call(xAxis);
+
+      const yScale = d3.scaleLinear().domain([0, fixedAxisLength]).range([0, -height]);
+      const yAxis = d3.axisLeft(yScale).ticks(fixedAxisLength);
+      coordinateSystem
+        .append('g')
+        .attr('class', 'y-axis')
+        .call(yAxis);
+
+      coordinateSystem
+        .append('circle')
+        .attr('cx', xScale(5))
+        .attr('cy', yScale(5))
+        .attr('r', 50)
+        .style('fill', 'red');
+
+      coordinateSystem
         .append('text')
-        .attr('x', 0)
-        .attr('y', 0)
-        .text(`Text option value: ${options.text}`);
+        .attr('x', xScale(1))
+        .attr('y', yScale(1))
+        .text(`Text option value: ${options.text}`)
+        .style('fill', 'gray');
     }
   }, [data, options, theme.colors.primary.main, width, height]);
 
@@ -71,7 +91,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         height={height}
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
+        viewBox={`0 0 ${width} ${height}`}
       />
     </div>
   );
